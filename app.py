e10abd009e9a7868cd6accd1e358351c25c5444b
Lua Zhi Zhan <zhizhanlua@gmail.com>
2018-01-17 12:55:19 +0800

    add uber and grab promo command

--- app.py
+++ app.py
@@ -21,9 +21,9 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 LOGGER = logging.getLogger(__name__)
 EMOJICODE = EmojiCode()
 
-TELEGRAM_TOKEN = 'TELEGRAM-TOKEN'
+TELEGRAM_TOKEN = ''
 PORT = int(os.environ.get('PORT', '5000'))
-BOTAN_TOKEN = 'BOTAN-API-KEY'
+BOTAN_TOKEN = ''
 
 # Define a few command handlers. These usually take the two arguments bot and
 # update. Error handlers also receive the raised TelegramError object in error.
@@ -42,6 +42,8 @@ def get_help(bot, update):
     msg += '/businfo - Show bus info' + EMOJICODE.bus() + EMOJICODE.info() + '\n'
     msg += '/emoji - Show emoji meaning ' +  EMOJICODE.smile() +'\n'
     msg += '/mrtmap - Show MRT map ' +  EMOJICODE.station() +'\n'
+    msg += '/grabpromo - Show grab promotions \n'
+    msg += '/uberpromo - Show uber promotions \n'
     msg += '/close - Close reply keyboard' + EMOJICODE.keyboard() +'\n'
     msg += '/help - Show help list' +  EMOJICODE.sos() +'\n'
     msg += '/start - To start from beginning\n'
@@ -68,7 +70,7 @@ def bus_stop(bot, update):
         msg = 'Tell me the bus stop no in this format ah. \n\n'
         msg += '*Format* : \n- /busstop <bus stop no> <bus no> (optional) \n\n'
         msg += '*Example* : \n- /busstop 67329 \n- /busstop 67329 163 \n\n'
-        msg += 'You can also send me a inline query by typing @oibuslehbot followed '
+        msg += 'You can also send me a inline query by typing @sgtravelkakibot followed '
         msg += 'by the bus stop name or code.'
         reply_markup = InlineKeyboardMarkup([
             [InlineKeyboardButton("Try inline query", switch_inline_query_current_chat='67321')]
@@ -121,8 +123,7 @@ def emoji_meaning(bot, update):
 def close_command(bot, update):
     """message send for Command: close"""
     botan.track(BOTAN_TOKEN, update.message.chat.id, update.message, '/close')
-    bot.send_message(chat_id=update.message.chat.id, text="Okay close liao.",
-    reply_markup=ReplyKeyboardRemove())
+    bot.send_message(chat_id=update.message.chat.id, text="Okay close liao.", reply_markup=ReplyKeyboardRemove())
 
 def inlinequery(bot, update):
     """Inline query message handling"""
@@ -142,6 +143,14 @@ def inlinequery(bot, update):
                 update.inline_query.answer(results)
                 break
         update.inline_query.answer(results)
+
+def grab_promo(bot, update):
+    msg = "*All Grab services* \n" 
+    bot.send_message(chat_id=update.message.chat.id, text=msg, parse_mode='Markdown')
+
+def uber_promo(bot, update):
+    msg = "*Uber Promotions* \n" 
+     bot.send_message(chat_id=update.message.chat.id, text=msg, parse_mode='Markdown')
 
 def echo(bot, update):
     """message send for user input"""
@@ -177,13 +186,11 @@ def inline_button(bot, update):
     bus = Bus()   
     if data_arr[0] == 'refresh':
         if len(data_arr) == 2:
-            botan.track(BOTAN_TOKEN, callback.message.chat.id, callback, 
-            'inline-btn-refresh-all')
+            botan.track(BOTAN_TOKEN, callback.message.chat.id, callback, 'inline-btn-refresh-all')
             msg = bus.get_bus_arrival_msg(data_arr[1])
             reply_markup = bus_arrival_markup(data_arr[1])
         else:
-            botan.track(BOTAN_TOKEN, callback.message.chat.id, callback, 
-            'inline-btn-refresh-single')
+            botan.track(BOTAN_TOKEN, callback.message.chat.id, callback, 'inline-btn-refresh-single')
             msg = bus.get_bus_arrival_msg(data_arr[1], data_arr[2])
             reply_markup = bus_arrival_markup(data_arr[1] + ' ' + data_arr[2])
         bot.editMessageText(chat_id=callback.message.chat.id,
@@ -202,8 +209,7 @@ def inline_button(bot, update):
         streetview = StreetView()
         bus_stop_detail = bus.get_bus_stop_detail(data_arr[1])
         if bus_stop_detail is not None:
-            botan.track(BOTAN_TOKEN, callback.message.chat.id, callback, 
-            'inline-btn-streetview')
+            botan.track(BOTAN_TOKEN, callback.message.chat.id, callback, 'inline-btn-streetview')
             latlng = streetview.get_lat_lng_str(bus_stop_detail)
             streetview_img_url = streetview.form_street_view_img_url(latlng)
             bot.send_photo(chat_id=callback.message.chat.id,
@@ -211,8 +217,7 @@ def inline_button(bot, update):
             bot.answerCallbackQuery(callback_query_id=callback.id,
             text='Bus stop photo shown')
         else:
-            botan.track(BOTAN_TOKEN, callback.message.chat.id, callback, 
-            'inline-btn-streetview-not-found')
+            botan.track(BOTAN_TOKEN, callback.message.chat.id, callback, 'inline-btn-streetview-not-found')
             bot.send_message(chat_id=callback.message.chat.id,
             message_id=callback.message.message_id, text='Bus stop not found leh.') 
             bot.answerCallbackQuery(callback_query_id=callback.id,
@@ -256,7 +261,7 @@ def main():
     # Create the EventHandler and pass it your bot's token.
     updater = Updater(TELEGRAM_TOKEN)
     updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TELEGRAM_TOKEN)
-    updater.bot.set_webhook("https://<app-name>.herokuapp.com/" + TELEGRAM_TOKEN)
+    updater.bot.set_webhook("https://app-name.herokuapp.com/" + TELEGRAM_TOKEN)
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
@@ -267,6 +272,8 @@ def main():
     dispatcher.add_handler(CommandHandler("nearby", nearby_bus_stop))
     dispatcher.add_handler(CommandHandler("businfo", bus_info))
     dispatcher.add_handler(CommandHandler("mrtmap", mrt_map))
+    dispatcher.add_handler(CommandHandler("grabpromo", grab_promo))
+    dispatcher.add_handler(CommandHandler("uberpromo", uber_promo))
     dispatcher.add_handler(CommandHandler("emoji", emoji_meaning))
     dispatcher.add_handler(CommandHandler("close", close_command))
     dispatcher.add_handler(CommandHandler("help", get_help))
